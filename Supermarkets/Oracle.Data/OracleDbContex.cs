42be5d757d013ac05bdd36b017b26e8f7e405d91
Martin Andreev <martin.andreev7@gmail.com>
2015-07-23 15:02:14 +0300

    Oracle Database

---
+++ Supermarkets/Oracle.Data/OracleDbContex.cs
@@ -0,0 +1,29 @@
+﻿namespace Oracle.Data
+{
+    using System.Data.Entity;
+    using Supermarket.Models;
+
+    public class OracleDbContex : DbContext
+    {
+        public OracleDbContex()
+            : base("OracleDbContext")
+        {
+        }
+
+         public IDbSet<Expense> Expenses { get; set; }
+        
+        public IDbSet<Product> Products { get; set; }
+        
+        public IDbSet<Sale> Sales { get; set; }
+        
+        public IDbSet<Supermarket> Supermarkets { get; set; }
+
+        public IDbSet<Vendor> Vendors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ANDREEV");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Supermarkets/SupermarketConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SupermarketConsoleClient
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using Oracle.Data;
+    using Supermarket.Models;
+
+    public class SupermarketMain
+    {
+        public static void Main()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<OracleDbContex>());
+            OracleDbContex contex = new OracleDbContex();
+
+            var supermarket = new Supermarket
+            {
+                Name = "Billa",
+            };
+
+            contex.Supermarkets.Add(supermarket);
+            contex.SaveChanges();
+
+            Console.WriteLine(contex.Supermarkets.Count());
+        }
+    }
+}
