77610c8f5518fb4fe6957c3d157d39d6c01a9592
Martin Andreev <martin.andreev7@gmail.com>
2015-07-26 19:09:58 +0300

    Export to XML implemented

---
+++ Supermarkets/MSSQL.Data/MSSQLRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace MSSQL.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using Utilities;
+
+    public static class MSSQLRepository
+    {
+        public static List<VendorSalesReport> GetSalesByVendors(DateTime startDate, DateTime endDate)
+        {
+            var context = new MSSQLContext();
+
+            var expenseGroups = context.Expenses
+                .Where(e => e.ExpenseDate > startDate && e.ExpenseDate < endDate)
+                .OrderBy(e => e.Vendor.Name)
+                .GroupBy(e => e.Vendor)
+                .Select(eg => new VendorReport
+                {
+                    VendorName = eg.Key.Name,
+                    Sales = eg
+                        .GroupBy(s => DbFunctions.TruncateTime(s.ExpenseDate))
+                        .Select(sg => new SaleReport
+                        {
+                            SaleDate = sg.Key,
+                            TotalSum = sg.Sum(s => s.ExpenseSum)
+                        }).ToList()
+                });
+
+            var vendorsSalesReport = new List<VendorSalesReport>();
+            vendorsSalesReport.AddRange(expenseGroups
+                .Select(@group => new VendorSalesReport
+                {
+                    Vendor = @group.VendorName,
+                    VendorReports = @group.Sales
+                }));
+
+            return vendorsSalesReport;
+        }
+    }
+}
---
+++ Supermarkets/MSSQL.Data/Utilities/VendorSalesReport.cs
@@ -0,0 +1,11 @@
+﻿namespace MSSQL.Data.Utilities
+{
+    using System.Collections.Generic;
+
+    public class VendorSalesReport
+    {
+        public string Vendor { get; set; }
+
+        public IEnumerable<SaleReport> VendorReports { get; set; } 
+    }
+}
