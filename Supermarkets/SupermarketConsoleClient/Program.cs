31a11367585e9e7d015c5ffe2a3e59d8e8a4e587
Plamen <pln@abv.bg>
2015-07-27 19:47:00 +0300

    Mongo And JSON

--- Supermarkets/SupermarketConsoleClient/Program.cs
+++ Supermarkets/SupermarketConsoleClient/Program.cs
@@ -1,69 +1,61 @@
-﻿namespace SupermarketConsoleClient
+﻿
+namespace SupermarketConsoleClient
 {
-    using System;
-    using System.Data.Entity;
-    using System.Linq;
-    using System.Transactions;
-    using System.Xml.Linq;
-    using MSSQL.Data;
-    using MSSQL.Data.Migrations;
-    using Supermarket.Models;
 
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using System;
+    using System.IO;
+    using MSSQL.Data;
 
     public class SupermarketMain
     {
         public static void Main()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MSSQLContext, Configuration>());
-            MSSQLContext context = new MSSQLContext();
+            string subDirectory = "../../Json-Reports";
 
-            XDocument xmlDoc = XDocument.Load("../../Sample-Vendor-Expenses.xml");
-            var vendors = xmlDoc.Descendants("vendor");
+            var client = new MongoClient();
+            var database = client.GetDatabase("Reports");
+            database.DropCollectionAsync("SalesByProductReports");
+            var dbReports = database.GetCollection<Reports>("SalesByProductReports");
 
-            foreach (var vendor in vendors)
+            DirectoryInfo di = new DirectoryInfo(subDirectory);
+            try
             {
-                string vendorName = vendor.Attribute("name").Value;
-                var wantedVendor = context.Vendors.FirstOrDefault(v => v.Name == vendorName);
+                di.Delete(true);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            di = Directory.CreateDirectory(subDirectory);
 
-                if (wantedVendor == null)
-                {
-                    var newVendor = new Vendor()
-                    {
-                        Name = vendorName
-                    };
+            var context = new MSSQLContext();
 
-                    context.Vendors.Add(newVendor);
-                    wantedVendor = newVendor;
-                }
+            string querySelect =
+                (@"SELECT 
+                    s.ProductId as product_id,
+                    p.Name as product_name,
+                    v.Name as vendor_name,
+                    SUM(s.Quantity) as total_quantity_sold, 
+                    SUM(s.Quantity * s.SalePrice) as total_incomes
+                FROM Sales s
+                INNER JOIN Products p ON p.Id = s.ProductId
+                INNER JOIN Vendors v ON v.Id = p.VendorId
+                GROUP BY ProductId, p.Name, v.Name");
 
-                var expenses = vendor.Descendants("expenses");
+            //WHERE startDate and endDate /|\
 
-                foreach (var expense in expenses)
-                {
-                    using (TransactionScope transaction = new TransactionScope())
-                    {
-                        try
-                        {
-                            decimal expenseSum = decimal.Parse(expense.Value);
-                            DateTime expenseDate = DateTime.Parse(expense.Attribute("month").Value);
-                            
-                            wantedVendor.Expenses.Add(new Expense()
-                            {
-                                ExpenseDate = expenseDate,
-                                ExpenseSum = expenseSum
-                            });
+            var reports = context.Database.SqlQuery<Reports>(querySelect);
 
-                            context.SaveChanges();
-                            Console.WriteLine(wantedVendor.Id);
-                            transaction.Complete();
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                            transaction.Dispose();
-                        }
-                    }
-                }
+            foreach (var report in reports)
+            {
+                string result = report.ToJson().Replace('_', '-');
+                File.WriteAllText("../../Json-Reports/" + report.product_id + ".json", result);
+
+                dbReports.InsertOneAsync(report).Wait();
+
+                Console.WriteLine(result);
             }
         }
     }
---
+++ Supermarkets/SupermarketConsoleClient/Reports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace SupermarketConsoleClient
+{
+    public class Reports
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public string vendor_name { get; set; }
+        public int total_quantity_sold { get; set; }
+        public decimal total_incomes { get; set; }
+
+    }
+}
